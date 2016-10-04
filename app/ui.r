7c308e715ba1342fd7d5f4a4c75a81829e70203b
Weichuan Wu <weichuan.wu@columbia.edu>
2016-10-04 17:19:46 -0400

    The intial version of code

--- app/ui.r
+++ app/ui.r
@@ -1,40 +1,49 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
 library(shiny)
+library(leaflet)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("2009 Manhattan Housing Sales"),
-  
-  # Sidebar with a selector input for neighborhood
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("nbhd", label = h5("Choose a Manhattan Neighborhood"), 
-                         choices = list("all neighborhoods"=0,
-                                        "Central Harlem"=1, 
-                                        "Chelsea and Clinton"=2,
-                                        "East Harlem"=3, 
-                                        "Gramercy Park and Murray Hill"=4,
-                                        "Greenwich Village and Soho"=5, 
-                                        "Lower Manhattan"=6,
-                                        "Lower East Side"=7, 
-                                        "Upper East Side"=8, 
-                                        "Upper West Side"=9,
-                                        "Inwood and Washington Heights"=10), 
-                         selected = 0)
-      #sliderInput("p.range", label=h3("Price Range (in thousands of dollars)"),
-      #            min = 0, max = 20000, value = c(200, 10000))
-    ),
-    # Show two panels
-    mainPanel(
-      #h4(textOutput("text")),
-      h3(code(textOutput("text1"))),
-      tabsetPanel(
-        # Panel 1 has three summary plots of sales. 
-        tabPanel("Sales summary", plotOutput("distPlot")), 
-        # Panel 2 has a map display of sales' distribution
-        tabPanel("Sales map", plotOutput("distPlot1")))
-    )
- )
+# Define UI for the Crime Map application
+shinyUI(navbarPage("Crime Map", id="nav",theme = "bootstrap.css",
+                   
+                   tabPanel("part1",
+                            sidebarLayout(position = "right", 
+                                          sidebarPanel(
+                                            h4("Filter"),
+                                            
+                                            # widget for crime type
+                                            checkboxGroupInput("Crime_Type", label = "Crime_Type",
+                                                               choices = c("BURGLARY", "FELONY ASSAULT", "GRAND LARCENY",
+                                                                           "GRAND LARCENY OF MOTOR VEHICLE", 
+                                                                           "RAPE", "ROBBERY"),
+                                                               selected = c("BURGLARY", "FELONY ASSAULT", "GRAND LARCENY",
+                                                                            "GRAND LARCENY OF MOTOR VEHICLE",
+                                                                            "RAPE","ROBBERY")),
+                                            
+                                            #date range
+                                            dateRangeInput("Date_Range", "Choose a date range", 
+                                                           start = "2015-10-01", end = "2015-12-31", 
+                                                           min = "2000-01-01", max = "2015-12-31"),
+                                            
+                                            #start and end hour
+                                            sliderInput("IntHour", "Start time", 0, 23, 0, step = 1),
+                                            sliderInput("EndHour", "End time", 0, 23, 23, step = 1),
+                                            
+                                            #update button
+                                            submitButton("Update"),
+                                            style = "opacity : 0.85"
+                                          ),
+                                          mainPanel(
+                                            leafletOutput("map", width = "100%", height = 700)
+                                          )
+                            )),
+                   tabPanel("part2"),
+                   tabPanel("part3")
 ))
-
